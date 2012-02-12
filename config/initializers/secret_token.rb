67e3842c5fe34db107799d994648700a1401a856
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-02-12 10:29:12 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FallingBlockRpgServer::Application.config.secret_token = '5880654a501ad810ca84ab517cf9f6d7c2e8bc871674e89c65d5e5a178714c67a0a04863bcd487e0ea1944789a9120049394c6dd7607f07d6c001a6d32498606'
