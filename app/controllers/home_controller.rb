cc9848de3b2246e982f113016e01a45ac278deff
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-10-01 08:34:39 -0400

    added code to keep track of winners

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,11 @@ class HomeController < ApplicationController
   def specs
     render :layout => false
   end
+
+  def player_wins
+    winner = Player.find_or_create_by_name(params[:winner])
+    loser = Player.find_or_create_by_name(params[:loser])
+    Outcome.create :winner => winner, :loser => loser
+    render :json => {:success => true}
+  end
 end
